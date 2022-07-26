f7d63e10f7b4f422a84fd7fe69a47a1598be60f4
dufan89 <79242739+dufan89@users.noreply.github.com>
2022-07-25 22:54:45 -0700

    Add files via upload

---
+++ hellocfc/build/CMakeFiles/hellocfc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hellocfc.dir/hellocfc.c.o"
+  "hellocfc.pdb"
+  "hellocfc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hellocfc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
